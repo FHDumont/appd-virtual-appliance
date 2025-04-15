1f18b0bd06952dd7cd92b2701f1f963aca3a9fa8
Fernando Dumont <fernando@fernando.com.br>
2025-04-15 15:00:13 -0300

    My settings

--- deploy/aws/02-aws-add-vpc.sh
+++ deploy/aws/02-aws-add-vpc.sh
@@ -8,12 +8,16 @@ source config.cfg
 
 
 # check if vpc exists
+echo "check if vpc exists"
 vpcID=$(aws --profile ${AWS_PROFILE} ec2 describe-vpcs --output text \
     --filters Name=tag-value,Values=${VPC_NAME} --query 'Vpcs[*].VpcId')
+# aws --profile va-deployment ec2 describe-vpcs --output text --filters Name=tag-value,Values=appd-va-vpc-1 --query 'Vpcs[*].VpcId'
+# vpc-07e8904051d6f0907
 
 if [ -z "$vpcID" ]; then
     echo "Did not find a vpc with name $VPC_NAME, creating"
     # create the vpc
+    echo "create the vpc"
     aws --profile ${AWS_PROFILE} ec2 create-vpc --cidr ${VPC_CIDR} \
         --tag-specifications "ResourceType=vpc,Tags=[{Key=Name,Value=${VPC_NAME}},${TAGS}]"
     if [ "$?" != 0 ]; then
@@ -21,10 +25,12 @@ if [ -z "$vpcID" ]; then
         exit 1
     fi
     sleep 5
+    echo "check if vpc exists"
     aws --profile ${AWS_PROFILE} ec2 describe-vpcs --output json --filters Name=tag-value,Values=${VPC_NAME}
 fi
 
 # get vpcID
+echo "get vpcID"
 vpcID=$(aws --profile ${AWS_PROFILE} ec2 describe-vpcs --output text --filters Name=tag-value,Values=${VPC_NAME} --query 'Vpcs[*].VpcId')
 if [ -z "$vpcID" ]; then
     echo "Did not find a vpc with name $VPC_NAME"
@@ -32,7 +38,10 @@ if [ -z "$vpcID" ]; then
 fi
 
 # create a subnet in the vpc
+echo "create a subnet in the vpc"
 subnetID=$(aws --profile ${AWS_PROFILE} ec2 describe-subnets --output text --filters Name=tag-value,Values="${SUBNET_NAME}" --query 'Subnets[*].SubnetId')
+# aws --profile va-deployment ec2 describe-subnets --output text --filters Name=tag-value,Values=appd-va-subnet-1 --query 'Subnets[*].SubnetId'
+# subnet-064d56222e2fb2ce3
 if [ -z "${subnetID}" ]; then
     echo "Did not find a subnet with name $SUBNET_NAME, creating"
     aws --profile ${AWS_PROFILE} ec2 create-subnet --cidr ${SUBNET_CIDR} \
@@ -44,12 +53,16 @@ if [ -z "${subnetID}" ]; then
         exit 1
     fi
     sleep 5
+    echo "create a subnet in the vpc"
     aws --profile ${AWS_PROFILE} ec2 describe-subnets --output json --filters Name=tag-value,Values="${SUBNET_NAME}"
     subnetID=$(aws --profile ${AWS_PROFILE} ec2 describe-subnets --output text --filters Name=tag-value,Values="${SUBNET_NAME}" --query 'Subnets[*].SubnetId')
 fi
 
 # create an internet gateway
+echo "create an internet gateway"
 igwID=$(aws --profile ${AWS_PROFILE} ec2 describe-internet-gateways --output text --filters Name=tag-value,Values=${IGW_NAME} --query 'InternetGateways[*].InternetGatewayId')
+# aws --profile va-deployment ec2 describe-internet-gateways --output text --filters Name=tag-value,Values=appd-va-inetgw-1 --query 'InternetGateways[*].InternetGatewayId'
+# igw-08e90d6eb4c42cd87
 if [ -z "$igwID" ]; then
     echo "Did not find an Internet Gateway with name $IGW_NAME, creating"
     aws --profile ${AWS_PROFILE} ec2 create-internet-gateway \
@@ -60,12 +73,16 @@ if [ -z "$igwID" ]; then
         exit 1
     fi
     sleep 5
+    echo "check an internet gateway"    
     aws --profile ${AWS_PROFILE} ec2 describe-internet-gateways --output json --filters Name=tag-value,Values=${IGW_NAME}
     igwID=$(aws --profile ${AWS_PROFILE} ec2 describe-internet-gateways --output text --filters Name=tag-value,Values=${IGW_NAME} --query 'InternetGateways[*].InternetGatewayId')
 fi
 
 # associate internet gateway to vpc
+echo "associate internet gateway to vpc"
 igw_VpcID=$(aws --profile ${AWS_PROFILE} ec2 describe-internet-gateways --output text --filters Name=tag-value,Values=${IGW_NAME}  --query 'InternetGateways[*].Attachments[*].VpcId')
+# aws --profile va-deployment ec2 describe-internet-gateways --output text --filters Name=tag-value,Values=appd-va-inetgw-1 --query 'InternetGateways[*].Attachments[*].VpcId'
+# vpc-07e8904051d6f0907
 if [ -z "$igw_VpcID" ] || [ "$igw_VpcID" != "$vpcID" ]; then
     echo "Did not find vpc associated with Internet Gateway $IGW_NAME, associating"
     aws --profile ${AWS_PROFILE} ec2 attach-internet-gateway \
@@ -74,11 +91,15 @@ if [ -z "$igw_VpcID" ] || [ "$igw_VpcID" != "$vpcID" ]; then
         echo "error attaching internet gateway ${igwID} to vpc ${vpcID}"
         exit 1
     fi
+    echo "check internet gateway to vpc"
     aws --profile ${AWS_PROFILE} ec2 describe-internet-gateways --output json --filters Name=tag-value,Values=${IGW_NAME}
 fi
 
 # create a route table
+echo "create a route table"
 rtID=$(aws --profile ${AWS_PROFILE} ec2 describe-route-tables --output text --filters Name=tag-value,Values=${RT_NAME} --query 'RouteTables[*].RouteTableId')
+# aws --profile va-deployment ec2 describe-route-tables --output text --filters Name=tag-value,Values=appd-va-rt-1 --query 'RouteTables[*].RouteTableId'
+# rtb-0ba4b21f97064a994
 if [ -z "$rtID" ]; then
     echo "Did not find a route table with the name $RT_NAME, creating"
     aws --profile ${AWS_PROFILE} ec2 create-route-table --vpc-id "${vpcID}" \
@@ -89,13 +110,20 @@ if [ -z "$rtID" ]; then
         exit 1
     fi
     sleep 5
+    echo "check a route table"
     aws --profile ${AWS_PROFILE} ec2 describe-route-tables --output json --filters Name=tag-value,Values=${RT_NAME}
     rtID=$(aws --profile ${AWS_PROFILE} ec2 describe-route-tables --output text --filters Name=tag-value,Values=${RT_NAME} --query 'RouteTables[*].RouteTableId')
 fi
 
 # add internet gateway to routing table
-mapfile -t routes < <(aws --profile ${AWS_PROFILE} ec2 describe-route-tables --output text --filters Name=tag-value,Values=${RT_NAME} --query 'RouteTables[*].Routes[*].DestinationCidrBlock')
-inarray=$(echo "${routes[@]}" | grep -o "0.0.0.0/0" | wc -w)
+echo "add internet gateway to routing table"
+# aws --profile va-deployment ec2 describe-route-tables --output text --filters Name=tag-value,Values=appd-va-rt-1 --query 'RouteTables[*].RouteTableId'
+# rtb-0ba4b21f97064a994
+# mapfile -t routes < <(aws --profile ${AWS_PROFILE} ec2 describe-route-tables --output text --filters Name=tag-value,Values=${RT_NAME} --query 'RouteTables[*].Routes[*].DestinationCidrBlock')
+routes=($(aws --profile ${AWS_PROFILE} ec2 describe-route-tables --output text --filters Name=tag-value,Values=${RT_NAME} --query 'RouteTables[*].Routes[*].DestinationCidrBlock'))
+
+# inarray=$(echo "${routes[@]}" | grep -o "0.0.0.0/0" | wc -w)
+inarray=$(printf "%s\n" "${routes[@]}" | grep -o "0.0.0.0/0" | wc -l)
 if [ "$inarray" != "1" ]; then
     echo "Did not find default route in routing able $RT_NAME, creating"
     aws --profile ${AWS_PROFILE} ec2 create-route \
@@ -106,11 +134,20 @@ if [ "$inarray" != "1" ]; then
         exit 1
     fi
     sleep 5
+    echo "check internet gateway to routing table"
     aws --profile ${AWS_PROFILE} ec2 describe-route-tables --output json --filters Name=tag-value,Values=${RT_NAME}
 fi
 
 # associate subnet with route table to access internet gateway
-mapfile -t assocSubnets < <(aws --profile ${AWS_PROFILE} ec2 describe-route-tables --output text --filters Name=tag-value,Values=${RT_NAME} --query 'RouteTables[*].Associations[*].SubnetId')
+echo "associate subnet with route table to access internet gateway"
+# mapfile -t assocSubnets < <(aws --profile ${AWS_PROFILE} ec2 describe-route-tables --output text --filters Name=tag-value,Values=${RT_NAME} --query 'RouteTables[*].Associations[*].SubnetId')
+assocSubnets=($(aws --profile ${AWS_PROFILE} ec2 describe-route-tables --output text --filters Name=tag-value,Values=${RT_NAME} --query 'RouteTables[*].Routes[*].DestinationCidrBlock'))
+
+# routes=($aws --profile va-deployment ec2 describe-route-tables --output text --filters Name=tag-value,Values=appd-va-rt-1 --query 'RouteTables[*].Routes[*].DestinationCidrBlock'))
+# 10.0.0.0/16 0.0.0.0/0
+# inarray=$(printf "%s\n" "${routes[@]}" | grep -o "0.0.0.0/0" | wc -l)
+# 1
+
 inarray=$(echo "${assocSubnets[@]}" | grep -i "${SUBNET_CIDR}" | wc -w)
 if [ "$inarray" != "1" ]; then
     echo "Did not find subnet $SUBNET_CIDR associated with routing table $RT_NAME, associating now"
@@ -120,10 +157,14 @@ if [ "$inarray" != "1" ]; then
         exit 1
     fi
     sleep 5
+    echo "check subnet with route table to access internet gateway"
     aws --profile ${AWS_PROFILE} ec2 describe-route-tables --output json --filters Name=tag-value,Values=${RT_NAME}
 fi
 
 # create a security group and rules
+echo "create a security group and rules"
+# aws --profile va-deployment ec2 describe-security-groups --output text --filters Name=tag-value,Values=appd-va-sg-1 --query 'SecurityGroups[*].GroupId'
+# sg-026333e5679d8f5ca
 sgID=$(aws --profile ${AWS_PROFILE} ec2 describe-security-groups --output text --filters Name=tag-value,Values=${SG_NAME} --query 'SecurityGroups[*].GroupId')
 if [ -z "$sgID" ]; then
     echo "Did not find a security group with the name $SG_NAME, creating"
@@ -137,14 +178,18 @@ if [ -z "$sgID" ]; then
         exit 1
     fi
     sleep 5
+    echo "check a security group and rules"
     aws --profile ${AWS_PROFILE} ec2 describe-security-groups --output json --filters Name=tag-value,Values=${SG_NAME}
     sgID=$(aws --profile ${AWS_PROFILE} ec2 describe-security-groups --output text --filters Name=tag-value,Values=${SG_NAME} --query 'SecurityGroups[*].GroupId')
 
 fi
 
 # create security group rules
+echo "# create security group rules"
 # check that we have rule for each vpn endpoint IP
-mapfile -t ipRules < <(aws --profile ${AWS_PROFILE} ec2 describe-security-groups --output text --filters Name=tag-value,Values=${SG_NAME} --query 'SecurityGroups[*].IpPermissions[*].IpRanges[*]' | sort | uniq | cut -f1)
+# mapfile -t ipRules < <(aws --profile ${AWS_PROFILE} ec2 describe-security-groups --output text --filters Name=tag-value,Values=${SG_NAME} --query 'SecurityGroups[*].IpPermissions[*].IpRanges[*]' | sort | uniq | cut -f1)
+ipRules=($(aws --profile ${AWS_PROFILE} ec2 describe-security-groups --output text --filters Name=tag-value,Values=${SG_NAME} --query 'SecurityGroups[*].IpPermissions[*].IpRanges[*]' | sort | uniq | cut -f1))
+# ipRules=($(aws --profile va-deployment ec2 describe-security-groups --output text --filters Name=tag-value,Values=appd-va-sg-1 --query 'SecurityGroups[*].IpPermissions[*].IpRanges[*]' | sort | uniq | cut -f1))
 for ((i=0; i<${#VPN_IPS[@]}; i++)); do
     vpnIP=${VPN_IPS[$i]}
     inarray=$(echo "${ipRules[@]}" | grep -o "$vpnIP" | wc -w)
@@ -163,7 +208,9 @@ for ((i=0; i<${#VPN_IPS[@]}; i++)); do
 done
 
 # create security group rule to allow traffic between instances in subnet
+echo "create security group rule to allow traffic between instances in subnet"
 inarray=$(echo "${ipRules[@]}" | grep -o "${SUBNET_CIDR}" | wc -w)
+# inarray=$(echo "${ipRules[@]}" | grep -o "10.0.0.0/24" | wc -w)
 if [ "${inarray}" != "1" ]; then
     echo "Adding ingress rule for ${SUBNET_CIDR}"
     aws --profile ${AWS_PROFILE} ec2 authorize-security-group-ingress \
@@ -179,8 +226,12 @@ else
 fi
 
 # create security group rule to allow traffic between instances in subnet
-mapfile -t ipEgressRules < <(aws --profile ${AWS_PROFILE} ec2 describe-security-groups --output text --filters Name=tag-value,Values=${SG_NAME} --query 'SecurityGroups[*].IpPermissionsEgress[*].IpRanges[*]' | sort | uniq | cut -f1)
+echo "create security group rule to allow traffic between instances in subnet"
+# mapfile -t ipEgressRules < <(aws --profile ${AWS_PROFILE} ec2 describe-security-groups --output text --filters Name=tag-value,Values=${SG_NAME} --query 'SecurityGroups[*].IpPermissionsEgress[*].IpRanges[*]' | sort | uniq | cut -f1)
+ipEgressRules=($(aws --profile ${AWS_PROFILE} ec2 describe-security-groups --output text --filters Name=tag-value,Values=${SG_NAME} --query 'SecurityGroups[*].IpPermissionsEgress[*].IpRanges[*]' | sort | uniq | cut -f1))
+# ipRules=($(aws --profile va-deployment ec2 describe-security-groups --output text --filters Name=tag-value,Values=appd-va-sg-1 --query 'SecurityGroups[*].IpPermissionsEgress[*].IpRanges[*]' | sort | uniq | cut -f1))
 
+# inarray=$(echo "${ipEgressRules[@]}" | grep -o "10.0.0.0/24" | wc -w)
 inarray=$(echo "${ipEgressRules[@]}" | grep -o "${SUBNET_CIDR}" | wc -w)
 if [ "${inarray}" != "1" ]; then
     echo "Adding egress rule for ${SUBNET_CIDR}"
--- deploy/aws/08-aws-create-vms.sh
+++ deploy/aws/08-aws-create-vms.sh
@@ -14,6 +14,8 @@ fi
 
 # get subnet id
 subnetID=$(aws --profile ${AWS_PROFILE} ec2 describe-subnets --output text --filters Name=tag-value,Values="${SUBNET_NAME}" --query 'Subnets[*].SubnetId')
+# aws --profile va-deployment ec2 describe-subnets --output text --filters Name=tag-value,Values=appd-va-subnet-1 --query 'Subnets[*].SubnetId'
+# subnet-064d56222e2fb2ce3
 if [ -z "${subnetID}" ]; then
     echo "Did not find a subnet with name $SUBNET_NAME, exiting"
     exit 1
@@ -21,6 +23,8 @@ fi
 
 # get security group id
 sgID=$(aws --profile ${AWS_PROFILE} ec2 describe-security-groups --output text --filters Name=tag-value,Values="${SG_NAME}" --query 'SecurityGroups[*].GroupId')
+# sgID=$(aws --profile va-deployment ec2 describe-security-groups --output text --filters Name=tag-value,Values="appd-va-sg-1" --query 'SecurityGroups[*].GroupId')
+# sg-026333e5679d8f5ca
 if [ -z "$sgID" ]; then
     echo "Did not find a security group with the name $SG_NAME, exiting"
     exit 1
@@ -47,26 +51,32 @@ EOF
                           --description "VA Network Interface" \
                           --groups "$sgID" \
                           --query 'NetworkInterface.NetworkInterfaceId' --output text)
+    # aws --profile va-deployment ec2 create-network-interface --subnet-id "subnet-064d56222e2fb2ce3" --description "VA Network Interface"  --groups "sg-026333e5679d8f5ca"  --query 'NetworkInterface.NetworkInterfaceId' --output text
+    # eni-04bf5a29b2573c29a
 
     # Allocate an Elastic IP
     allocation_id=$(aws --profile ${AWS_PROFILE} ec2 allocate-address \
                         --domain vpc \
                         --query 'AllocationId' --output text)
+    # aws --profile va-deployment ec2 allocate-address  --domain vpc  --query 'AllocationId' --output text
+    # eipalloc-009984565c49aeca3
 
     # Associate the Elastic IP with the ENI
     aws ec2 --profile ${AWS_PROFILE} associate-address \
             --allocation-id ${allocation_id} \
             --network-interface-id ${network_intf_id}
+    # aws ec2 --profile va-deployment associate-address  --allocation-id eipalloc-009984565c49aeca3 --network-interface-id eni-04bf5a29b2573c29a
+    # eipassoc-0f39f5c76358133f5
 
     # requires Nitro instance types
     aws --profile ${AWS_PROFILE} ec2 run-instances \
         --image-id "$AMI_ID" \
-	--instance-type "${VM_TYPE}" \
+	    --instance-type "${VM_TYPE}" \
         --network-interfaces "[{\"NetworkInterfaceId\":\"${network_intf_id}\",\"DeviceIndex\":0}]" \
         --block-device-mappings \
         "DeviceName=/dev/sda1,Ebs={VolumeSize=${VM_OS_DISK},VolumeType=gp3}" \
         "DeviceName=/dev/sdb,Ebs={VolumeSize=${VM_DATA_DISK},VolumeType=gp3,DeleteOnTermination=false}" \
-	--user-data file://user-data.ec2 \
+	    --user-data file://user-data.ec2 \
         --no-cli-pager \
         --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${VM_NAME}},${TAGS}]" 
 
